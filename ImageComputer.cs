69249d39fb291832ffd4abd74bb28494dc1da7b3
Mingqian Ye <mingqian.ye@gmail.com>
2023-10-16 23:42:48 -0700

    work

---
+++ ImageComputer.cs
@@ -0,0 +1,7 @@
+using Godot;
+
+public static class ImageComputer {
+  public static Image Interpolate(Image Image, int Cols) {
+    return Image;
+  }
+}
--- Main.cs
+++ Main.cs
@@ -4,15 +4,30 @@ using System;
 public partial class Main : Node2D
 {
   private Image img;
+  private int spriteWidth;
 	public override void _Ready()
 	{
     this.GetNode<Button>("VBoxContainer/ImportBtn").Connect("pressed", Callable.From(() => {
-      this.GetNode<FileDialog>("FileDialog").PopupCentered();
+      this.GetNode<FileDialog>("OpenFileDialog").PopupCentered();
     }));
 
-    this.GetNode<FileDialog>("FileDialog").Connect("file_selected", Callable.From<string>(path => {
+    this.GetNode<FileDialog>("OpenFileDialog").Connect("file_selected", Callable.From<string>(path => {
       this.img = Image.LoadFromFile(path);
       this.GetNode<Label>("VBoxContainer/PngInfo").Text = imgInfo(img);
+      this.GetNode<Button>("VBoxContainer/ExportBtn").Disabled = false;
+    }));
+
+    this.GetNode<SpinBox>("VBoxContainer/HSplitContainer/WidthInputBox").Connect("value_changed", Callable.From<float>(num => {
+      this.spriteWidth = (int) num;
+    }));
+
+    this.GetNode<Button>("VBoxContainer/ExportBtn").Connect("pressed", Callable.From(() => {
+      this.GetNode<FileDialog>("SaveFileDialog").PopupCentered();
+    }));
+
+    this.GetNode<FileDialog>("SaveFileDialog").Connect("file_selected", Callable.From<string>(path => {
+      this.img.SavePng(path);
+      this.GetNode<Label>("VBoxContainer/ExportInfo").Text = $"Exported to: {path}";
     }));
 	}
 
