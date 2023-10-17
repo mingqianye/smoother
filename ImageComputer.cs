47aca93e27302c4867d7451c7475d9b78f1977c7
Mingqian Ye <mingqian.ye@gmail.com>
2023-10-17 01:49:37 -0700

    work

--- ImageComputer.cs
+++ ImageComputer.cs
@@ -1,7 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using Godot;
+using LanguageExt;
+using static LanguageExt.Prelude;
 
 public static class ImageComputer {
-  public static Image Interpolate(Image Image, int Cols) {
-    return Image;
+  public static Image Interpolate(Image Image, int SpriteWidth, int Factor) {
+    var newRows = toRows(Image, SpriteWidth).Map(r => interpolate(r, Factor));
+    return toImage(newRows);
   }
+
+  private static Arr<Row> toRows(Image image, int spriteWidth) {
+    List<Row> rows = new List<Row>();
+
+    for (int row = 0; row < image.GetHeight(); row++) {
+      List<Block> blocks = new List<Block>();
+
+      for (int blockId = 0; blockId < image.GetWidth() / spriteWidth; blockId++) {
+        List<Color> colors = new List<Color>();
+
+        for (int i = 0; i < spriteWidth; i++) {
+          colors.Add(image.GetPixel(blockId * spriteWidth + i, row));
+        }
+
+        blocks.Add(new Block(colors.ToArr()));
+      }
+      rows.Add(new Row(blocks.ToArr()));
+    }
+
+    return rows.ToArr();
+  }
+
+
+  private static Image toImage(Arr<Row> rows) {
+    Image img = Image.Create(
+      width: rows[0].Blocks[0].Colors.Count * rows[0].Blocks.Count,
+      height: rows.Count,
+      useMipmaps: false,
+      format: Image.Format.Rgba8);
+
+    int blockWidth = rows[0].Blocks[0].Colors.Count;
+    
+    for (int row = 0; row < rows.Count; row++){
+      for (int blockId = 0; blockId < rows[row].Blocks.Count; blockId++) {
+        for (int i = 0; i < blockWidth; i++) {
+          img.SetPixel(i + blockWidth * blockId, row, rows[row].Blocks[blockId].Colors[i]);
+        }
+      }
+    }
+    
+    return img;
+  }
+
+  private static Row interpolate(Row row, int factor) {
+    if (factor == 0) return row;
+    var newBlocks = row.Blocks
+      .Zip(row.Blocks.Skip(1))
+      .Map(pair => interpolate(pair, factor))
+      .SelectMany(x => x)
+      .Append(row.Blocks.Last())
+      .ToArr();
+    return new Row(newBlocks);
+  }
+
+  private static Arr<Block> interpolate((Block, Block) pair, int factor) {
+    Block step = (pair.Item2 - pair.Item1) / factor;
+
+    return Enumerable.Range(0, factor + 1)
+      .Map(i => pair.Item1 + step * i)
+      .ToArr();
+  }
+
+  private record Row(Arr<Block> Blocks);
+  private record Block(Arr<Color> Colors) {
+
+    public static Block operator +(Block a, Block b) {
+      var colors = a.Colors
+        .Zip(b.Colors)
+        .Map(pair => pair.Item1 + pair.Item2)
+        .ToArr();
+      return new Block(colors);
+    }
+
+    public static Block operator -(Block a, Block b) {
+      var colors = a.Colors
+        .Zip(b.Colors)
+        .Map(pair => pair.Item1 - pair.Item2)
+        .ToArr();
+      return new Block(colors);
+    }
+
+    public static Block operator *(Block a, float i) {
+      var colors = a.Colors
+        .Map(c => c * i)
+        .ToArr();
+      return new Block(colors);
+    }
+
+    public static Block operator /(Block a, float i) {
+      return a * (1/i);
+    }
+
+  };
 }
--- Main.cs
+++ Main.cs
@@ -4,7 +4,7 @@ using System;
 public partial class Main : Node2D
 {
   private Image img;
-  private int spriteWidth;
+  private int spriteWidth, factor;
 	public override void _Ready()
 	{
     this.GetNode<Button>("VBoxContainer/ImportBtn").Connect("pressed", Callable.From(() => {
@@ -14,6 +14,11 @@ public partial class Main : Node2D
     this.GetNode<FileDialog>("OpenFileDialog").Connect("file_selected", Callable.From<string>(path => {
       this.img = Image.LoadFromFile(path);
       this.GetNode<Label>("VBoxContainer/PngInfo").Text = imgInfo(img);
+
+      SpinBox sb = this.GetNode<SpinBox>("VBoxContainer/HSplitContainer/WidthInputBox");
+      sb.MinValue = 1;
+      sb.MaxValue = img.GetWidth() / 2;
+
       this.GetNode<Button>("VBoxContainer/ExportBtn").Disabled = false;
     }));
 
@@ -21,13 +26,18 @@ public partial class Main : Node2D
       this.spriteWidth = (int) num;
     }));
 
+    this.GetNode<SpinBox>("VBoxContainer/HSplitContainer2/FactorInputBox").Connect("value_changed", Callable.From<float>(num => {
+      this.factor = (int) num;
+    }));
+
     this.GetNode<Button>("VBoxContainer/ExportBtn").Connect("pressed", Callable.From(() => {
       this.GetNode<FileDialog>("SaveFileDialog").PopupCentered();
     }));
 
     this.GetNode<FileDialog>("SaveFileDialog").Connect("file_selected", Callable.From<string>(path => {
-      this.img.SavePng(path);
-      this.GetNode<Label>("VBoxContainer/ExportInfo").Text = $"Exported to: {path}";
+      Image newImage = ImageComputer.Interpolate(this.img, this.spriteWidth, this.factor);
+      newImage.SavePng(path);
+      this.GetNode<Label>("VBoxContainer/ExportInfo").Text = $"Exported to: {path}. {imgInfo(newImage)}";
     }));
 	}
 
